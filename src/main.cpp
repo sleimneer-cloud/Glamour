ff3b87e4d4050478d4a4ab393292ccdfcc9b8d1a
HuiSeok Kim <sleimneer@gmail.com>
2026-04-09 15:24:19 +0900

    윈도우용 키보드 적용

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@
 int main(int argc, char *argv[])
 {
 
-    // 🌟 우분투 ibus 꼬리물기 버그 해결을 위한 환경 변수 세팅
-    // ibus가 글자를 비동기로 처리하지 않고, 즉시 꽂아넣도록 설정합니다.
+// 🌟 우분투 ibus 꼬리물기 버그 해결을 위한 환경 변수 세팅
+// ibus가 글자를 비동기로 처리하지 않고, 즉시 꽂아넣도록 설정합니다.
+#ifdef Q_OS_LINUX // 리눅스일때만 적용
     qputenv("QT_IM_MODULE", QByteArray("ibus"));
     qputenv("IBUS_ENABLE_SYNC_MODE", QByteArray("1")); // 👈 이 줄이 핵심입니다! 동기 모드 강제.
+#endif
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
